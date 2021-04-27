623885934701788e3ea4b2eb580e3686065ec13c
Theboys619 <Theboys619@gmail.com>
2021-04-27 21:37:09 +0000

    Added subroutine calls and returning.

--- main.go
+++ main.go
@@ -3,13 +3,16 @@ package main
 import (
 	// "fmt"
 	// "main/compiler"
+	// "fmt"
 	"main/ivm"
 )
 
 func main() {
 	// fmt.Println("Hello, World!")
 	vm := ivm.NewVM(256)
-	vm.LoadProgram([]uint8{
+	prog := make([]uint8, 0, 256)
+	
+	prog = append(prog, []uint8{
 		ivm.PUSHL, 0x00, 0x02,
 		ivm.PUSHL, 0x00, 0x02,
 		ivm.ADD,
@@ -17,15 +20,26 @@ func main() {
 		ivm.PUSHR, 0x00,
 		ivm.PUSHREGV, 0x00,
 		ivm.MOVL, 0x04, 0x00, 0x06,
-		ivm.PUSHL, 0x00, 0x00,
-		ivm.CALL, 0x00, 0x18,
+		ivm.MOVL, 0x05, 0x00, 0x0a,
+		ivm.PUSHL, 0x00, 0x07,
+		ivm.PUSHL, 0x00, 0x01,
+		ivm.CALL, 0x00, 0x64,
+		ivm.PUSHL, 0x00, 0x0c,
 		ivm.HALT,
+	}...)
+
+	subroutine1 := []uint8{
 		ivm.PUSHL, 0x00, 0x03,
 		ivm.PUSHL, 0x00, 0x03,
 		ivm.ADD,
-		ivm.POP, 0x05,
+		ivm.POP, 0x0a,
 		ivm.RET,
-	})
+	}
+
+	prog = append(prog, prog[len(prog):100]...)
+	prog = append(prog, subroutine1...)
+
+	vm.LoadProgram(prog)
 	vm.Run()
 
 	// lexer := compiler.NewLexer()
